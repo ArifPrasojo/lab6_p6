bb52def4c8865c868871c14ba8be42df149f13ca
username Anda <arifprasojo999@email.com>
2024-10-24 00:51:08 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Praktikum Flutter 06 | Layout dan Navigasi
+
+## Identitas
+- **NIM**: 2241760100
+- **Nama**: Arif Prasojo.
+- **Kelas**: SIB - 3E
+
+## Hasil Akhir 
+
+### 1. **Praktikum 1: Membangun Layout di Flutter**
+1. Buka file main.dart lalu ganti dengan kode berikut. Isi nama dan NIM Anda di text title.
+![Text Widget](img/P1.png)
+2. Implementasi title row : Pertama, Anda akan membuat kolom bagian kiri pada judul. Tambahkan kode berikut di bagian atas metode build() di dalam kelas MyApp:
+![Text Widget](img/P1.1.png)
+### 2. **Text Widget**
+![Text Widget](images/02.png)
+### 3. **Image Widget**
+![Text Widget](images/03.png)
+### 4. **Cupertino Button dan Loading Bar**
+![Text Widget](images/04.png)
+### 5. **Floating Action Button (FAB)**
+![Text Widget](images/05.png)
+### 6. **Scaffold Widget**
+![Text Widget](images/06.png)
+### 7. **Dialog Widget**
+![Text Widget](images/07.png)
+### 8. **Input dan Selection Widget**
+![Text Widget](images/08.png)
+### 9. **Date and Time Pickers**
+![Text Widget](images/09.png)
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Menggunakan MaterialApp sebagai aplikasi utama
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Wisata Gunung'),
+        ),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Widget titleSection yang berisi struktur Row dan Column untuk title
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    return ListView(
+      children: [
+        titleSection,
+      ],
+    );
+  }
+}
